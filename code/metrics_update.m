1cb586480e4015025eb25ac4590afdae286efad1
Steffen Arnold <steffen.arnold@verticube.net>
2015-12-09 23:24:25 +0100

    Count reproduction number only from recovered.

--- code/metrics_update.m
+++ code/metrics_update.m
@@ -12,8 +12,7 @@ M.numRecovered(t,s)   = length(find(S.Disease.recovered));
 %
 % Mean over numbers of infections caused by single node
 %
-numEverInfected = S.Topology.numNodes-M.numSusceptible(t,s);
-M.meanR0(t,s) = sum(S.Disease.infections)/numEverInfected;
+M.meanR0(t,s) = mean(S.Disease.infections(S.Disease.recovered > 0));
 
 
 end

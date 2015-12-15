d7b2721759f34b9570c547f2228d19dd4e057841
C-nit <joriscadow@gmail.com>
2015-12-15 05:44:15 +0100

    bugfix edges; reduce redundancy

--- code/append_graph.m
+++ code/append_graph.m
@@ -21,25 +21,34 @@ edges_end = '    </edges>';
 graph_end = '  </graph>';
 gexf_end = '</gexf>';
 
+
+%nodes--------
 fid = fopen(sprintf('Gephi/dynamic_graph_run%d.gexf',run), 'a');
 fprintf(fid,'%s\n',nodes_start);
     for id = 1:id_max
         node_open = sprintf('      <node id="%d">', id);
         fprintf(fid,'%s\n',node_open );
         fprintf(fid,'%s\n',at_open );
-        node_ks = sprintf('          <attvalue for="knowledge" value="%4.3f" start="%d"></attvalue>\n', ...
-            [ks(id,:);(1:t_max)] );
-        fprintf(fid,node_ks);
-        node_sir_states = sprintf('          <attvalue for="state" value="%d" start="%d"></attvalue>\n', ...
-            [sirs(id,:);(1:t_max)] );
-        fprintf(fid,node_sir_states);
+            for i = (find(ks(id,:),1,'first'):t_max ) %
+                node_ks = sprintf('          <attvalue for="knowledge" value="%4.3f" start="%d"></attvalue>\n', ...
+                    [ks(id,i);i] );
+                fprintf(fid,node_ks);
+            end
+            %fprintf(fid,'          <attvalue for="knowledge" value="0.0" start="%d"></attvalue>\n', max(i)+1);
+            for j = [find((sirs(id,:) == 1), 1, 'first'),find((sirs(id,:) == 2), 1, 'first'),find((sirs(id,:) == 3), 1, 'first') ]
+                node_sir_states = sprintf('          <attvalue for="state" value="%d" start="%d"></attvalue>\n', ...
+                    [sirs(id,j);j] );
+                fprintf(fid,node_sir_states);
+            end
         fprintf(fid,'%s\n',at_close);
         fprintf(fid,'%s\n',node_close);
     end
 fprintf(fid,'%s\n',nodes_end);
-%todo edges -----------------
+fclose(fid);
+%edges -----------------
+fid = fopen(sprintf('Gephi/dynamic_graph_run%d.gexf',run), 'a');
 fprintf(fid,'%s\n',edges_start);
-    for edge_id = 1:new_edges(:,1)
+    for edge_id = 1:max(size(new_edges(:,1)))
         edge_open = sprintf('      <edge id="%d" source="%d" target="%d" start="%d" end="%d">\n',...
             new_edges(edge_id,:), new_edges(edge_id,4)+1 );
         fprintf(fid, edge_open);
